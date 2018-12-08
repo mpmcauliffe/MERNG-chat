09e15f2d731a6a424be5a48919169828d3e2968e
mpmcauliffe <mcauliffe99@gmail.com>
2018-12-07 23:26:20 -0500

    update

--- index.js
+++ index.js
@@ -6,3 +6,43 @@ const express                  = require('express'),
 
 app.listen(3000, () => console.log('. . .up on 3000. . .'))
 
+db = {
+    users: [
+        { id: '1', email: 'bear@snuggy.com', name: 'Jimbo' },
+        { id: '2', email: 'cuteBear@bear.com', name: 'Maddy' },
+        { id: '3', email: 'bossBear@bear.com', name: 'Leo' }
+    ]
+}
+
+const schema = buildSchema(`
+    type Query {
+        users: [User!]!
+    }
+    type User {
+        id: ID!
+        email: String!
+        name: String
+        avatarUrl: String
+    }
+`)
+
+const rootValue = {
+    users: () => db.users
+}
+
+graphql(
+    schema,
+    `
+        {
+            users {
+                id
+                email
+            }
+        }
+    `,
+    rootValue
+).then(
+    res => console.dir(res, { depth: null })
+).catch(
+    console.error
+)
