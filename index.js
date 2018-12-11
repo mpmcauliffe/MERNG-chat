b82d3a7b2b0a4e02675e4764efa211efd7c9b9fa
mpmcauliffe <mcauliffe99@gmail.com>
2018-12-10 19:44:35 -0500

    basic message functionality

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 const express                  = require('express'),
       graphqlHTTP              = require('express-graphql'),
       { graphql, buildSchema } = require('graphql'),
+      crypto                   = require('crypto'),
       app                      = express();
 
 
 db = {
     users: [
-        { id: '1', email: 'bear@snuggy.com', name: 'Jimbo' },
-        { id: '2', email: 'cuteBear@bear.com', name: 'Maddy' },
-        { id: '3', email: 'bossBear@bear.com', name: 'Leo' }
+        { id: '1', email: 'bear@snuggy.com', name: 'Jimbo', avatarUrl: 'https://giantBear.com' },
+        { id: '2', email: 'cuteBear@bear.com', name: 'Maddy', avatarUrl: 'https://prettyBear.com' },
+        { id: '3', email: 'bossBear@bear.com', name: 'Leo', avatarUrl: 'https://funnyBear.com' }
+    ],
+    messages: [
+        { id: '1', userId: '1', body: 'I want cake', createdAt: Date.now() },
+        { id: '2', userId: '1', body: 'I want attention', createdAt: Date.now() },
+        { id: '3', userId: '1', body: 'I want my ball', createdAt: Date.now() }
     ]
 }
 
 const schema = buildSchema(`
     type Query {
         users: [User!]!
+        user(id: ID!): User
+        messages: [Message!]!
+    }
+    type Mutation {
+        addUser(email: String!, name: String): User
     }
     type User {
         id: ID!
@@ -22,10 +33,27 @@ const schema = buildSchema(`
         name: String
         avatarUrl: String
     }
+    type Message {
+        id: ID!
+        body: String!
+        createdAt: String
+    }
 `)
 
 const rootValue = {
-    users: () => db.users
+    users: () => db.users,
+    user: args => db.users.find(user => user.id === args.id),
+    messages: () => db.messages,
+    addUser: ({ email, name }) => {
+        const user = {
+            id:  crypto.randomBytes(10).toString('hex'),
+            email,
+            name
+        }
+        db.users.push(user)
+        
+        return user
+    }
 }
 
 
@@ -36,21 +64,3 @@ app.use('/graphql', graphqlHTTP({
 }))
 
 app.listen(3000, () => console.log('. . .up on 3000. . .'))
-
-
-// graphql(
-//     schema,
-//     `
-//         {
-//             users {
-//                 id
-//                 email
-//             }
-//         }
-//     `,
-//     rootValue
-// ).then(
-//     res => console.dir(res, { depth: null })
-// ).catch(
-//     console.error
-// )
